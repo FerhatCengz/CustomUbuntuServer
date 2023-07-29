0fc40e9daae5023f97279af4c21de46b1784b798
Ferhat <79553498+FerhatCengz@users.noreply.github.com>
2023-07-29 20:12:56 +0300

    Add project files.

---
+++ API/ProductController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UbuntuTrial.API
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductController : ControllerBase
+    {
+        List<string> products = new List<string>();
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            products.Add("Telefon");
+            products.Add("PC");
+            return Ok(products);
+        }
+
+
+
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace UbuntuTrial.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
